4bb4b7172c37406986e0d5daeb95cb9cfb7e5c06
nlrb <github@ramonbaas.nl>
2016-04-03 21:53:52 +0200

    Add settings, improve panel pairing, change panel id

--- drivers/powermax/driver.js
+++ drivers/powermax/driver.js
@@ -8,10 +8,10 @@ var self = module.exports = {
 	
 	init: function(devices, callback) {
 		devices.forEach(function(device) {
-			// TODO Get device settings
-			var ip_port = device.id.split(":");
-			pm.addPanel(ip_port[0], ip_port[1]);
-			pm.addPanelActions(self, device);
+			self.getSettings(device, function(err, settings){
+				pm.addPanel(self, device, settings);
+				//pm.addPanelActions(self, device);
+			})
 		});
 		
 		// we're ready
@@ -43,31 +43,52 @@ var self = module.exports = {
 		pm.deletePanel(device_data.id);
 	},
 	
+	settings: function(device_data, newSettingsObj, oldSettingsObj, changedKeysArr, callback) {
+		// run when the user has changed the device's settings in Homey.
+		// changedKeysArr contains an array of keys that have been changed, for your convenience :)
+		pm.updateSettings(device_data.id, changedKeysArr, newSettingsObj);
+		// always fire the callback, or the settings won't change!
+		// if the settings must not be saved for whatever reason:
+		// callback( "Your error message", null );
+		// else
+		callback(null, true);
+	},
+	
 	pair: function(socket) {
 		Homey.log('PowerMax panel pairing has started...');
+		var completed = false;
 		var panel_id;
 
 		// Search for the PowerMax once we received IP address and port
 		socket.on('search', function(data, callback) {
 			panel_id = data.ip + ':' + data.port;
 			pm.debug('Request to search for PowerMax on ' + panel_id);
-			pm.addPanel(data.ip, data.port);
+			// Add default settings
+			var settings = {
+				ip: data.ip,
+				port: Number(data.port),
+				allowArm: 'all',
+				motionTime: 8,
+				syncTime: true
+			}
+			pm.addPanel(self, null, settings);
 			callback(null, true);
 		});
 		
-		// Return configuration data to front-end
-		socket.on('getDetails', function(data, callback) {
-			var result = pm.getPanelDetails(panel_id);
-			callback(null, result);
-		});
-		
 		// Fully add panel when successful
-		socket.on('add_device', function(device_data, callback) {
-			var device = device_data['data'];
-			pm.addPanelActions(self, device_data);
-			callback();
+		socket.on('completed', function(device_data) {
+			var device = device_data.data;
+			completed = true;
+			pm.addPanelActions(self, device);
 		});
 		
+		// Check if the pairing was finished, otherwise remove the panel
+		socket.on('disconnect', function() {
+			if (!completed) {
+				pm.deletePanel(panel_id);
+			}
+		});
+
 		// Notify the front-end if a PowerMax has been found
 		Homey.on('found', function(data) {
 			socket.emit('found', data);
@@ -75,6 +96,7 @@ var self = module.exports = {
 
 		// Notify the front-end on enrollment/download progress
 		Homey.on('download', function(data) {
+			panel_id = data.id;
 			socket.emit('download', data);
 		});
 	}
